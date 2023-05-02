4c4e575a9c04174b7a728a9ac522edc64ab4b441
Erbol Betenov <betenov64@mail.ru>
2023-05-02 23:05:08 +0600

    Create Edit input

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Task } from "../types";
 
 interface Props {
@@ -7,11 +7,39 @@ interface Props {
 }
 
 const TodoList = ({ task, removeTask }: Props) => {
+  const [editTask, setEditTask] = useState(task.task);
+  const [editInput, setEditInput] = useState(false);
+
   return (
     <div className="border border-black-400 rounded-md w-3/6 m-auto p-2 text-lg flex justify-between mb-6">
-      <span>{task.task}</span>
+      {editInput ? (
+        <input
+          className="w-4/6"
+          value={editTask}
+          autoFocus
+          type="text"
+          onChange={(event) => setEditTask(event.target.value)}
+        />
+      ) : (
+        <div className="mt-3">{editTask}</div>
+      )}
 
-      <button onClick={removeTask}>X</button>
+      <div className="flex justify-between w-1/4 items-center ">
+        <button
+          onClick={removeTask}
+          className="bg-blue-500 mt-3 mb-3 hover:bg-blue-400 text-white font-normal
+       py-1 px-2 text-xs border-b-3 border-blue-700 self-start  hover:border-blue-500 rounded"
+        >
+          Delete
+        </button>
+        <button
+          onClick={() => setEditInput((prev) => !prev)}
+          className="bg-blue-500 mt-3 mb-3 hover:bg-blue-400 text-white font-normal
+       py-1 px-2 text-xs border-b-3 border-blue-700   hover:border-blue-500 rounded"
+        >
+          {editInput ? "Save" : "Edit"}
+        </button>
+      </div>
     </div>
   );
 };
